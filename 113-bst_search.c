e81a7e1d3a3044f742ee6b22a848de1bc8bacc5d
odelolajosh <oladapoodelola@gmail.com>
2022-08-26 06:16:41 +0100

    fix: lint code with betty

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -10,10 +10,10 @@ bst_t *bst_search(const bst_t *tree, int value)
 {
 	if (!tree)
 		return (NULL);
-	
+
 	if (value == tree->n)
 		return ((bst_t *)tree);
-	
+
 	if (value < tree->n)
 		return (bst_search(tree->left, value));
 	return (bst_search(tree->right, value));
